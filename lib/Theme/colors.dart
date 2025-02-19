fca52449f207091190c3239d39b45c502915e492
Anuj Lowanshi <anujlowanshi3@gmail.com>
2025-02-19 11:34:51 +0530

    Theme setup
    Added main (r) file and related colors and assers file

--- lib/Theme/colors.dart
+++ lib/Theme/colors.dart
@@ -1,9 +1,6 @@
 
 part of 'r.dart';
 
-
-
-
 class _ResColors {
   Color primary=Colors.blue;
   Color secondary=Colors.lightBlueAccent;
--- lib/Theme/r.dart
+++ lib/Theme/r.dart
@@ -1,4 +1,4 @@
-library r;
+library;
 import 'package:flutter/material.dart';
 part 'assets.dart';
 part 'colors.dart';
@@ -6,12 +6,6 @@ class R {
   R._();
 
   static final _ResColors colors = _ResColors();
-  // static final _ResBorders borders = _ResBorders();
-  // static final _ResFonts fonts = _ResFonts();
-  // static final _ResDecorations decorations = _ResDecorations();
-  // static final _ResDimensions dimensions = _ResDimensions();
   static final _ResAssets assets = _ResAssets();
-  // static final _ResRadius radius = _ResRadius();
-  // static final _ResTranslation translation = _ResTranslation();
-  // static final _Validation validation = _Validation();
+
 }
